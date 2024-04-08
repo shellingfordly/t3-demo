54dcb428bbca54cd82f16ba1cdbe2dd5a7ef02b4
shellingfordly <shellingfordly@qq.com>
2024-04-08 16:48:34 +0800

    feat: add clerk auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { type AppType } from "next/app";
 import { Inter } from "next/font/google";
-
+import { ClerkProvider } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
@@ -12,7 +12,9 @@ const inter = Inter({
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <main className={`font-sans ${inter.variable}`}>
-      <Component {...pageProps} />
+      <ClerkProvider {...pageProps}>
+        <Component {...pageProps} />
+      </ClerkProvider>
     </main>
   );
 };
